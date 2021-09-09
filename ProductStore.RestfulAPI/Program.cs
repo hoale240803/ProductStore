715390eac7211c0a07b18caa8baff03acd821b65
lehoa08121998@gmail.com <lehoa08121998@gmail.com>
2021-09-09 23:49:32 +0700

    add docker file

--- ProductStore.RestfulAPI/Program.cs
+++ ProductStore.RestfulAPI/Program.cs
@@ -11,10 +11,10 @@ namespace ProductStore.RestfulAPI
 {
     public class Program
     {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+        //public static void Main(string[] args)
+        //{
+        //    CreateHostBuilder(args).Build().Run();
+        //}
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
